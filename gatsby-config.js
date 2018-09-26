a9adf32993a4e27134fa513b399b14e88e6b38ae
lrivett-we <luke.rivett@we.org>
2018-09-26 11:35:04 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: 'gatsby-source-storyblok',
+      options: {
+        accessToken: 'yrJkpc0qoC1bybxzfzRtkwtt',
+        homeSlug: 'home',
+        version: 'draft'
+      }
+    }
+  ]
+}
